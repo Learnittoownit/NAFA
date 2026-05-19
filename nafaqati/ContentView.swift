075a30e6a686dc6880366546a8903f6d1e5feeab
Rq <iwliluqp@gmail.com>
2026-05-19 15:09:15 +0300

    Initial Commit

---
+++ nafaqati/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nafaqati
+//
+//  Created by Rama AlQahtani on 02/12/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nafaqati/nafaqatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nafaqatiApp.swift
+//  nafaqati
+//
+//  Created by Rama AlQahtani on 02/12/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct nafaqatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
