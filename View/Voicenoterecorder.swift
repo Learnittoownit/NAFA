fd407391204dae8250a0805eb7291fc4005f83ec
Rq <iwliluqp@gmail.com>
2026-06-04 16:43:10 +0300

    rama work is done

---
+++ View/Voicenoterecorder.swift
@@ -0,0 +1,18 @@
+//
+//  Voicenoterecorder.swift
+//  nafaqati
+//
+//  Created by Rama AlQahtani on 18/12/1447 AH.
+//
+
+import SwiftUI
+
+struct Voicenoterecorder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Voicenoterecorder()
+}
